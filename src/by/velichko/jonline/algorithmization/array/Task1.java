d3b22b0845de0928656423ac82bd41157cbb5a3c
Valentin Velichko <velichko1882@gmail.com>
2020-07-21 01:36:24 +0300

    Commit 2: add Task 1 in Algorithmization-array

---
+++ src/by/velichko/jonline/algorithmization/array/Task1.java
@@ -0,0 +1,14 @@
+package by.velichko.jonline.algorithmization.array;
+
+/*
+В массив А[N] занесены натуральные числа. Найти сумму тех элементов,
+которые кратны данному К.
+*/
+public class Task1 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
